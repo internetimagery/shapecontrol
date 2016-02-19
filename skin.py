c49633dc53db1969ae82547d68b0d2fb7b4ed72a
Jason <jason.dixon.email@gmail.com>
2016-02-19 23:34:43 +1300

    adding demo test sphere

---
+++ skin.py
@@ -0,0 +1,9 @@
+# Skin and joint related functionality
+
+import maya.cmds as cmds
+
+if __name__ == '__main__':
+    cmds.file(new=True, force=True) # New blank scene for testing
+    sphere = cmds.polySphere()[0] # Test Sphere
+    jnt1, jnt2 = ((cmds.select(cl=True), cmds.joint(p=a))[1] for a in ((0,-1,0), (0,1,0))) # Add Joints
+    skin = cmds.skinCluster(sphere, jnt1, jnt2)[0] # Add skin to sphere
