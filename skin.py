c915f9599c9a1912c85b85956c0d97b43d6a8717
Jason <jason.dixon.email@gmail.com>
2016-02-19 23:41:17 +1300

    get skin

--- skin.py
+++ skin.py
@@ -2,8 +2,14 @@
 
 import maya.cmds as cmds
 
+def get_skin(joint):
+    for skin in cmds.listConnections(joint, s=False, type="skinCluster") or []:
+        return skin
+
 if __name__ == '__main__':
     cmds.file(new=True, force=True) # New blank scene for testing
     sphere = cmds.polySphere()[0] # Test Sphere
     jnt1, jnt2 = ((cmds.select(cl=True), cmds.joint(p=a))[1] for a in ((0,-1,0), (0,1,0))) # Add Joints
     skin = cmds.skinCluster(sphere, jnt1, jnt2)[0] # Add skin to sphere
+    # Testing
+    assert get_skin(jnt1) == skin
